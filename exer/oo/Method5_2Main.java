a0c264fdbb583b5b2e6dd4916f27b6f9bf2aac8d
HyungCheolSim <sim34122@gmail.com>
2023-04-12 20:46:20 +0900

    java handbook chap5 objectoriented study until method

---
+++ exer/oo/Method5_2Main.java
@@ -0,0 +1,37 @@
+package exer.oo;
+
+class Method5_2 {
+    void gugudan(int dan) {
+        if (!(dan >= 2 && dan <= 9)) {
+            System.out.println(dan + "단은 없습니다.");
+            return;
+        }
+        System.out.println(dan + " 단 시작!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(dan + "*" + i + " = " + dan * i);
+        }
+        System.out.println();
+    }
+    boolean checkMax(int x, int y){
+        if(x>y){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+public class Method5_2Main {
+    public static void main(String[] args) {
+        Method5_2 method52 = new Method5_2();
+
+        method52.gugudan(2);
+        method52.gugudan(5);
+        method52.gugudan(10);
+        method52.gugudan(9);
+
+        System.out.println("mehtod52.checkMax(10, 8) = "+ method52.checkMax(10,8));
+        System.out.println("mehtod52.checkMax(5, 9) = "+ method52.checkMax(5,9));
+
+    }
+}
---
+++ exer/oo/NoneClassTime.java
@@ -0,0 +1,15 @@
+package exer.oo;
+
+public class NoneClassTime {
+    public static void main(String[] args){
+        //총 3명의 시간을 변수로 관리
+        int hour1,hour2,hour3;
+        int minute1,minute2,minute3;
+        int second1,second2,second3;
+
+        //총 3명의 시간을 배열로 관리
+        int[] hour = new int[3];
+        int[] minute = new int[3];
+        int[] second = new int[3];
+    }
+}
